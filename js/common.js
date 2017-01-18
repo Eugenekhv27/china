ecc726841002c58325215b064f76b27196bbded2
Eugene <evnikolaevv@gmail.com>
2017-01-18 14:14:27 +0300

    first

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(function() {
+$('#slider').crossSlide({
+sleep: 2,
+
+fade: 1
+}, [
+{ src: 'img/slid2.jpg' },
+{ src: 'img/slid3.jpg' },
+{ src: 'img/slid4.jpg' },
+{ src: 'img/slid5.jpg' }
+]);
+
+});
